315d94a917ababc1ab3a7c190ec26f00210c1791
Isaac <you@example.com>
2022-11-16 16:20:29 +0200

    style the footer

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -9,7 +9,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_YOUTUBE_KEY,
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
   },
 };
